223a7f1a0f67a8dc68c72aecde2c65ec81692c83
ankit25032 <githubankit222@gmail.com>
2021-10-03 07:18:49 +0530

    first

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from "nextjs-cors";
+import ytdl from "ytdl-core";
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  const data = await ytdl.getInfo(
+    "https://www.youtube.com/watch?v=n0r2rENgvwY"
+  );
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(data);
 }
